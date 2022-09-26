70dd9176e4e8e6da32a8a551fe0d4f4702e65381
Srdjan <srdjancoralic5@gmail.com>
2022-09-26 10:30:28 +0200

    Add stripe client secret

--- functions/create-payment-intent.ts
+++ functions/create-payment-intent.ts
@@ -1,10 +1,36 @@
 // domain/.netlify/functions/create-payment-intent
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
 
 exports.handler = async function (event, context) {
-  console.log(event);
+  if (event.body) {
+    const { cart, shippingFee, totalAmount } = JSON.parse(event.body);
 
+    const calculateOrderAmonut = () => {
+      // normaly, here the backend logic and connection
+      // to our own API would be handled
+      return shippingFee + totalAmount;
+    };
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmonut(),
+        currency: 'usd',
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ msg: error.message }),
+      };
+    }
+  }
   return {
     statusCode: 200,
-    body: 'payment intent',
+    body: 'Create Payment Intent',
   };
 };
